3f607964446b9875990d1fbd638a3e0306c70be1
model-labmeg <andre.valente@ifrj.edu.br>
2026-06-09 11:06:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -103,4 +103,6 @@ def balanco():
         return jsonify({'status': 'erro', 'mensagem': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
